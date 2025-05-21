58fa724c5ff989944c159420d3ce1cfbb0f95cda
Thanh1610 <Tatuanthanh2105nd@gmail.com>
2025-05-21 12:19:23 +0700

    ENV environment

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,9 +1,8 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
-import 'dotenv/config';
-
+import { env } from './environment';
 let trelloDB = null;
 
-const client = new MongoClient(process.env.MONGODB_URI, {
+const client = new MongoClient(env.MONGODB_URI, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
@@ -13,10 +12,14 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 
 export const CONNECT_DB = async () => {
     await client.connect();
-    trelloDB = client.db(process.env.DATABASE_NAME);
+    trelloDB = client.db(env.DATABASE_NAME);
 };
 
 export const GET_DB = () => {
     if (!trelloDB) throw new Error('Must connect to Database first!');
     return trelloDB;
+};
+
+export const CLOSE_DB = async () => {
+    await client.close();
 };
--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
+import exitHook from 'exit-hook';
 import express from 'express';
-import { CONNECT_DB, GET_DB } from '~/config/mongodb';
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb';
+import { env } from '~/config/environment';
 
 const START_SERVER = () => {
     const app = express();
 
-    const hostname = 'localhost';
-    const port = 3001;
-
     app.get('/', async (req, res) => {
-        console.log(await GET_DB().listCollections().toArray());
+        // console.log(await GET_DB().listCollections().toArray());
         res.end('<h1>Hello World!</h1><hr>');
     });
 
-    app.listen(port, hostname, () => {
-        console.log(`3.Example app listening on port ${port}`);
+    app.listen(env.APP_PORT, env.APP_HOST, () => {
+        console.log(`3.Example app listening on port ${env.APP_PORT}`);
+    });
+
+    exitHook(() => {
+        console.log('Exiting');
+        CLOSE_DB();
     });
 };
 
